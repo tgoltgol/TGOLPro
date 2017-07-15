4004fef0accca37eaa75ffe9e085fbe736be54d7
Pansystar <wh.bt3.p2@gmail.com>
2017-07-15 19:07:35 +0900

    Add : 新規作成

---
+++ Assets/Game/Player.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	static readonly int MaxMoney = 1000000000;
+
+	[SerializeField]
+	int _Money;
+
+	[SerializeField]
+	string _Name;
+
+
+
+	public string Name { 
+		get {
+			return _Name;
+		}
+		set {
+			_Name = value;
+		}
+	}
+	public int Money {
+		get {
+			return _Money;
+		}
+		set {
+			_Money = value;
+			if (_Money > MaxMoney) {
+				_Money = MaxMoney;
+			}
+		}
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
