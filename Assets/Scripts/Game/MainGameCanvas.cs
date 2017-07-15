fc4ae47f4599017ecef6d4a1183389ea3e9b356f
矢澤亮太 <klsh72@yahoo.co.jp>
2017-07-16 00:43:01 +0900

    [Map]map生成基盤作成

---
+++ Assets/Scripts/Game/MainGameCanvas.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainGameCanvas : MonoBehaviour
+{
+
+	/// <summary>mapオブジェクトリスト</summary>
+	[SerializeField]
+	private List<GameObject> mapList;
+	/// <summary>使用するSpriteのList</summary>
+	[SerializeField]
+	private List<Sprite> spriteList;
+
+	void Start ()
+	{
+		//生成されたマップリストにspriteを設定
+		//後々ここでマップ情報セット
+		for (int i = 0; i < mapList.Count; i++) {
+			this.mapList [i].GetComponent<SpriteRenderer> ().sprite = this.spriteList [Random.Range (0, 10)];
+		}
+	}
+}
